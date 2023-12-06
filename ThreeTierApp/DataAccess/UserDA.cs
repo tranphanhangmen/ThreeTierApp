e505e992c333665649fd29047c0cc507780c3b73
tran men <kiet9305@gmail.com>
2023-12-06 15:17:03 +0700

    ThreeTierApp

---
+++ ThreeTierApp/DataAccess/UserDA.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data; // Required for using Dataset , Datatable and Sql  
+using System.Data.SqlClient; // Required for Using Sql  
+using System.Configuration; // for Using Connection From Web.config   
+using BussinessObject;  // for acessing bussiness object class  
+
+namespace DataAccess
+{
+    public class UserDA
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Myconstr"].ToString());
+        public int AddUserDetails(UserBO ObjBO) // passing Bussiness object Here  
+        {
+            try
+            {
+                /* Because We will put all out values from our (UserRegistration.aspx) To in Bussiness object and then Pass it to Bussiness logic and then to DataAcess  this way the flow carry on*/
+                SqlCommand cmd = new SqlCommand("Data Source=LAPTOP-R1Q9EQQN\\MSSQL2016;Initial Catalog=UserDB;User ID=sa;password=123open", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", ObjBO.Name);
+                cmd.Parameters.AddWithValue("@Address", ObjBO.address);
+                cmd.Parameters.AddWithValue("@EmailID", ObjBO.EmailID);
+                cmd.Parameters.AddWithValue("@Mobilenumber", ObjBO.Mobilenumber);
+                con.Open();
+                int Result = cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return Result;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ThreeTierApp/ThreeTierApp/Userregistration.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ThreeTierApp
+{
+    public partial class Userregistration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+        override protected void OnInit(EventArgs e)
+        {
+            BtnSave.Click += new EventHandler(BtnSave_Click);
+
+            InitializeComponent();
+            base.OnInit(e);
+        }
+        private void InitializeComponent()
+        {
+            this.Load += new System.EventHandler(this.Page_Load);
+        }
+    }
+}
