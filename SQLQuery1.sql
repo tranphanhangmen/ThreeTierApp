6d74fb6340faace82089e19bd0a6a41668b7eca3
tran men <kiet9305@gmail.com>
2023-12-07 21:27:55 +0700

    up

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+USE [UserDB]
+GO
+
+SELECT [id]
+      ,[Name]
+      ,[Address]
+      ,[EmailID]
+      ,[Mobilenumber]
+      ,[country]
+      ,[sex]
+      ,[dbo]
+  FROM [dbo].[Userinfo]
+GO
+
